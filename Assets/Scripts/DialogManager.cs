59d5581f503e8ae59adf4fedf71e3df6b5c67f2d
unknown <leonidb93@gmail.com>
2019-08-31 23:00:33 +0200

    make sound work again, add AI enemies

---
+++ Assets/Scripts/DialogManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DialogManager : MonoBehaviour
+{
+    public GameObject[] AllDialoges;
+    public string[] AllTexts;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        StartCoroutine(AssignDialoges());
+    }
+
+    private IEnumerator AssignDialoges() {
+        WaitForSeconds wait = new WaitForSeconds(4f);
+        int i = 0;
+        foreach (GameObject dialog in AllDialoges) {
+            dialog.GetComponentInChildren<TextMeshProUGUI>().text = AllTexts[i];
+            i++;
+            yield return wait;
+        }
+    }
+    
+}
